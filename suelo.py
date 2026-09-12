ceca2a64ddcfa2ccf3df021a519f4cfcec28c8fe
miguel robles <miguel.robles@atmosfera.unam.mx>
2026-09-11 19:49:58 -0600

    added RTC v0.1 (test0)

--- suelo.py
+++ suelo.py
@@ -1,24 +1,43 @@
 import time, ds18x20
-from machine import Pin, ADC
+from machine import Pin, ADC, I2C, RTC
 import onewire
+import ds3231
 
 pines = {}
 pines['led'] = 13
 pines['tmp'] = 15
-pines['adc'] = [ 4, 12, 34 ]
+pines['adc'] = [ 4, ]
 print('iniciando...')
 
 led = Pin( pines['led'], Pin.OUT)
 led.off()
 adc = []
+
+print('Inicializando canales analógicos')
 for a in pines['adc']:
     adc.append( ADC(a, atten = ADC.ATTN_11DB ) )
+
+print('Inicializando sensor de temperatura')
 ow = onewire.OneWire(Pin(pines['tmp']))
 ow.reset()             
-
 ds = ds18x20.DS18X20(ow)
 roms = ds.scan()
 print(roms)
+
+print('Inicializando I2C')
+i2c = I2C(0, scl=Pin(22, Pin.OPEN_DRAIN, Pin.PULL_UP),
+              sda=Pin(21, Pin.OPEN_DRAIN, Pin.PULL_UP), freq=100000)
+print('i2c:', i2c.scan() )
+rtc = RTC()
+print(rtc.datetime())
+
+print ('Leyendo reloj:' )
+ans = ds3231.get_time(i2c)
+print(ans)
+
+print( 'ADC0,Temperatura')
+time.sleep(3)
+
 while (True):
     ds.convert_temp()
     time.sleep_ms(750)
